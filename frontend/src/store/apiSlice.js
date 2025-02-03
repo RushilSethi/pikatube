3975fb44343445efa2f7fc12c03415ddc16dea5d
Rushil <rushilsethi333@gmail.com>
2025-02-03 23:16:25 +0530

    modified frontend to connect to the hosted backend api

--- frontend/src/store/apiSlice.js
+++ frontend/src/store/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:1000/",
+    baseUrl: import.meta.env.VITE_BACKEND_URL,
     prepareHeaders: (headers, { getState, endpoint }) => {
       const token = localStorage.getItem("AuthToken");
 
