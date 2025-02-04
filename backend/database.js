6ce943165e2f782070943222d481c5cfc0c63fc3
Rushil <rushilsethi333@gmail.com>
2025-02-04 13:29:14 +0530

    added cors middleware for frontend domain

--- backend/database.js
+++ backend/database.js
@@ -21,7 +21,13 @@ db.on("error", () => {
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://pikatube.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.listen(process.env.PORT, () => {
   console.log("Server running on port ", process.env.PORT);
 });
